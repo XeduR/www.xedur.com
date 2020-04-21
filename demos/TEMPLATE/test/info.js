59e96c5a93488dd2edc13b738c81f5c268aa7005
XeduR <eetu.rantanen@spyric.com>
2020-04-21 12:40:07 +0300

    Test

---
+++ demos/TEMPLATE/test/info.js
@@ -0,0 +1,2 @@
+var title = "PROJECT TITLE";
+var description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br><br><br>Nulla tristique neque id ante vulputate, ut blandit libero consequat.";
