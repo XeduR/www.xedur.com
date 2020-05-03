9a24dbcfbd2530eca813a246240dcc6af4c9d3c9
XeduR <eetu.rantanen@spyric.com>
2020-05-03 23:44:42 +0300

    Koodihaasteen lopullinen versio

--- demos/koodihaaste/js/routePlanner.js
+++ demos/koodihaaste/js/routePlanner.js
@@ -1,128 +1,129 @@
+// Luodaan taulukko vierekkäisistä pysäkeistä, joiden välillä on bussiyhteys.
+function getConnections( routes ) {
+    let connections = {};
+    for (let i in routes.linjastot) {
+        connections[i] = {};
+        for (let j = 0; j < routes.linjastot[i].length-1; j++) {
+            connections[i][routes.linjastot[i][j] + routes.linjastot[i][j+1]] = true;
+            connections[i][routes.linjastot[i][j+1] + routes.linjastot[i][j]] = true;
+        }
+    }
+    return connections;
+}
+
+// Valmistellaan reittioppaan data Djikstran algoritmia varten.
 function prepareRoutes( routes ) {
     var busStops = {};
-    for (let i = 0; i < routes.pysäkit.length; i++) {
+    for (let i = 0; i < routes.pysakit.length; i++) {
         busStops[routes.pysakit[i]] = {};
     }
     
-    // let connections = {};
-    // for (let i in routes.linjastot) {
-    //     connections[i] = {};
-    //     for (let j = 0; j < routes.linjastot.length-1; j++) {
-    //         connections[i][routes.linjastot[i][j] + routes.linjastot[i][j+1]] = true;
-    //         connections[i][routes.linjastot[i][j+1] + routes.linjastot[i][j]] = true;
-    //     }
-    // }
-    // 
-    // for (let i = 0; i < routes.tiet.length; i++) {
-    //     let duration = routes.tiet[i].kesto;
-    //     let from = routes.tiet[i].mista;
-    //     let to = routes.tiet[i].mihin;
-    // 
-    //     let segment = from + to;
-    //     let isValid = false;
-    //     for (let j in connections) {
-    //         if (typeof j[segment] !== 'undefined') {
-    //             isValid = true;
-    //             break;
-    //         }
-    //     }
-    // 
-    //     if (isValid) {
-    //         busStops[from][to] = duration;
-    //         busStops[to][from] = duration;
-    //     }
-    // }
+    for (let i = 0; i < routes.tiet.length; i++) {
+        let duration = routes.tiet[i].kesto;
+        let from = routes.tiet[i].mista;
+        let to = routes.tiet[i].mihin;
     
+        let segment = from + to;
+        let isValid = false;
+        for (let j in connections) {
+            if (typeof connections[j][segment] !== 'undefined') {
+                isValid = true;
+                break;
+            }
+        }
+    
+        if (isValid) {
+            busStops[from][to] = duration;
+            busStops[to][from] = duration;
+        }
+    }
+    
+    // console.log( busStops );
     return busStops;
 }
 
+// Djiksan algoritmia käyttäen, määritetään nopein reitti jokaiselle pysäkille
+// annetun lähtöpysäkin ja määränpään perusteella.
+function getRoute( map, start, destination ) {    
+    let path = {};
+    let visited = {};
+    
+    for (let index in map) {
+        path[index] = {};
+        path[index].d = Infinity;
+        visited[index] = false;
+    }
+    path[start].d = 0;
+    
+    let current = start;
+    
+    // Käydään jokainen pysäkki ja niiden naapurit läpi ja selvitetään nopein reitti jokaiselle pysäkille.
+    while (true) {
+        let nearest, nearestIndex;
+        
+        for (let index in map[current]) {
+            if (visited[index] === false) {
+                let newDistance = path[current].d + map[current][index];                
+                
+                if (newDistance < path[index].d) {
+                    path[index].d = newDistance;
+                    path[index].previous = current;
+                }
+                
+                if (typeof nearest === 'undefined' || path[index].d < nearest) {
+                    nearest = path[index].d;
+                    nearestIndex = index;
+                }
+            }
+        }
+    
+        if (typeof nearest !== 'undefined') {
+            visited[current] = true;
+            current = nearestIndex;
+        } else {
+            // Huom! Tätä voisi parantaa jonkun verran, jos algoritmiä ei ajettaisi pysäkkien
+            // kohdalla jos määränpää on jo saavutettu ja kyseiselle pysäkille on pidempi kesto
+            // kuin jo saavutettuun määränpäähän. Tämän koodihaasteen kohdalla kyseinen parannus
+            // jäi tekemättä.
+            nearest = Infinity;
+            
+            for (let index in visited) {
+                if (visited[index] == false) {
+                    if (path[index].d < nearest) {
+                        nearest = path[index].d;
+                        nearestIndex = index;
+                    }
+                }
+            }
 
-
-
-function getRoute( map, start, destination ) {
-    // if not busStops[start] or not busStops[destination] then
-    //     print( "\nVaroitus: pysäkkiä \"" .. (not busStops[start] and start or destination) .. "\" ei ole olemassa." )
-    //     return false
-    // end
-    // 
-    // local path, visited = {}, {}
-    // for index, _ in pairs( map ) do
-    //     path[index] = { d=_huge }
-    //     visited[index] = false
-    // end
-    // path[start] = { d=0 }
-    // 
-    // local current = start
-    // 
-    // repeat
-    //     local nearest, nearestIndex
-    // 
-    //     for index, distance in pairs( map[current] ) do
-    //         if not visited[index] then
-    //             local newDistance = path[current].d + distance
-    //             if newDistance < path[index].d then
-    //                 path[index] = { d=newDistance, previous=current }
-    //             end
-    //             if not nearest then
-    //                 nearest = path[index].d
-    //                 nearestIndex = index
-    //             elseif path[index].d < nearest then
-    //                 nearest = path[index].d
-    //                 nearestIndex = index
-    //             end
-    //         end
-    //     end
-    // 
-    //     if nearest then
-    //         visited[current] = true
-    //         current = nearestIndex
-    //     else
-    //         nearest = _huge
-    // 
-    //         for i, j in pairs( visited ) do
-    //             if not j then
-    //                 if path[i].d < nearest then
-    //                     nearest = path[i].d
-    //                     nearestIndex = i
-    //                 end
-    //             end
-    //         end
-    // 
-    //         if nearestIndex then
-    //             visited[current] = true
-    //             current = nearestIndex
-    //         else
-    //             nearest = nil
-    //         end
-    //     end
-    // until not nearest
-    // 
+            if (typeof nearestIndex !== 'undefined') {
+                visited[current] = true;
+                current = nearestIndex;
+            } else {
+                break;
+            }
+        }
+    }
     
+    // Kun nopein reitti määränpäähän on tiedossa, niin selvitetään
+    // vielä reitti takaisin alkupisteeseen. Tämän jälkeen reitti
+    // käännetään vielä takaisin "alku -> loppu" järjestykseen.
+    let stops = [];
+    let durations = [];
+    let next = destination;
+    let n = 0;
     
-    // -- Poimitaan Djikstran algoritmilla laskettu optimaalinen reitti.
-    // local function getShortestRoute( nodes, destination )
-    //     local route, next = {}, destination
-    // 
-    //     repeat
-    //         route[#route+1] = { next, nodes[next].d }
-    //         next = nodes[next].previous
-    //     until not next
-    // 
-    //     return route
-    // end
+    while (true) {
+        stops[n] = next;
+        durations[n] = path[next].d;
+        next = path[next].previous;
+        n++;
+        if (!next) {break};
+    }
+    let shortestRoute = {};
+    shortestRoute.stops = stops.reverse();
+    shortestRoute.duration = durations.reverse();
+    // console.log( shortestRoute );
     
-    
-    // -- printSegments( path, true )
-    // return getShortestRoute( path, destination )
+    return shortestRoute;
 }
-
-
-
-// if travelRoute then
-//     local stringPath = ""
-//     for i = #travelRoute, 1, -1 do
-//         stringPath = stringPath .. travelRoute[i][1] .. ", "
-//     end
-//     stringPath = stringPath:sub(1,-3)
-//     print( "\nOptimaalinen reitti on " .. stringPath .. ", kesto on " .. travelRoute[1][2] .. "." )
-// end
