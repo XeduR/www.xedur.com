500102e7fc29ba36db4221e2ddfe3ecc39a0a260
Eetu Rantanen <eetu.rantanen@spyric.com>
2021-06-24 19:51:46 +0300

    Update random.js
    Update comments and add Math.abs() to ensure that the seed is positive.

--- demos/Pseudorandom Number Generator/js/random.js
+++ demos/Pseudorandom Number Generator/js/random.js
@@ -1,4 +1,11 @@
 // Pseudorandom number generation using the linear congruential method:
+///////////////////////////////////////////////////////////////////////
+
+// An interactive online tool demonstrating the random distribution is available at:
+// https://www.xedur.com/demos/Pseudorandom%20Number%20Generator/
+
+// A pure Lua implementation of this same linear congruential method is available at:
+// https://github.com/XeduR/Solar2D-Projects/blob/master/Pseudorandom%20Number%20Generator/rng.lua
 
 // Initial randomisation parameters (you can leave these as is).
 var a = 1664525;
@@ -9,16 +16,16 @@ var seed = 12345;
 // Set a new initial random seed.
 function randomseed(n) {
     if (typeof(n) == "number") {
-        seed = Math.floor(n+0.5);
+        seed = Math.floor(Math.abs(n+0.5));
     }
 }
 
-// If only one arguments is passed, then x >= 1. If two arguments are passed, then y >= x.
+// Generate a pseudorandom number.
 function random(x,y) {
     seed = (a * seed + c) % m;
     let r = seed / m;
-    // With no arguments, return a random number between 0 and 1.
-    // With one argument, return a random number between it and 1.
-    // With two arguments, return a random number between them.
+    // With no arguments,  return a pseudorandom number (fraction) between 0 and 1.
+    // With one argument,  return a pseudorandom number (integer)  between 1 and x.
+    // With two arguments, return a pseudorandom number (integer)  between x and y.
     return !(typeof(x) == "number") ? r : !(typeof(y) == "number") ? Math.floor((x-1)*r+1.5) : Math.floor((y-x)*r+x+0.5);
 }
