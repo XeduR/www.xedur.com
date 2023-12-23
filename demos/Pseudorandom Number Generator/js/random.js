83914b6032713c070b826d7ab9df487e166a5d02
Eetu Rantanen <eetu@erantanen.com>
2023-12-23 22:15:27 +0200

    random.js: switch spaces to tabs, change comment

--- demos/Pseudorandom Number Generator/js/random.js
+++ demos/Pseudorandom Number Generator/js/random.js
@@ -1,11 +1,12 @@
+/////////////////////////////////////////////////////////////////////////
 // Pseudorandom number generation using the linear congruential method:
-///////////////////////////////////////////////////////////////////////
-
-// An interactive online tool demonstrating the random distribution is available at:
+//
+// You can see the pseudorandom number genration in action over at:
 // https://www.xedur.com/demos/Pseudorandom%20Number%20Generator/
-
-// A pure Lua implementation of this same linear congruential method is available at:
+//
+// A pure Lua implementation of this file is available at:
 // https://github.com/XeduR/Solar2D-Projects/blob/master/Pseudorandom%20Number%20Generator/rng.lua
+/////////////////////////////////////////////////////////////////////////
 
 // Initial randomisation parameters (you can leave these as is).
 var a = 1664525;
@@ -15,17 +16,17 @@ var seed = 12345;
 
 // Set a new initial random seed.
 function randomseed(n) {
-    if (typeof(n) == "number") {
-        seed = Math.floor(Math.abs(n+0.5));
-    }
+	if (typeof(n) == "number") {
+		seed = Math.floor(Math.abs(n+0.5));
+	}
 }
 
 // Generate a pseudorandom number.
 function random(x,y) {
-    seed = (a * seed + c) % m;
-    let r = seed / m;
-    // With no arguments,  return a pseudorandom number (fraction) between 0 and 1.
-    // With one argument,  return a pseudorandom number (integer)  between 1 and x.
-    // With two arguments, return a pseudorandom number (integer)  between x and y.
-    return !(typeof(x) == "number") ? r : !(typeof(y) == "number") ? Math.floor((x-1)*r+1.5) : Math.floor((y-x)*r+x+0.5);
+	seed = (a * seed + c) % m;
+	let r = seed / m;
+	// With no arguments,  return a pseudorandom number (fraction) between 0 and 1.
+	// With one argument,  return a pseudorandom number (integer)  between 1 and x.
+	// With two arguments, return a pseudorandom number (integer)  between x and y.
+	return !(typeof(x) == "number") ? r : !(typeof(y) == "number") ? Math.floor((x-1)*r+1.5) : Math.floor((y-x)*r+x+0.5);
 }
