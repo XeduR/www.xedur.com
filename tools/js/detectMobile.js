9a6f4eeda8546ed48f655915915454c68dff43ff
Eetu Rantanen <eetu@erantanen.com>
2026-04-20 14:09:02 +0300

    Add The Hunt for Red April demo

--- tools/js/detectMobile.js
+++ tools/js/detectMobile.js
@@ -1,3 +1,5 @@
+// Detect touch-capable devices, but filter out filter out
+// laptops with touchscreens (which still have a mouse).
 window.mobileCheck = function() {
     return (
         navigator.maxTouchPoints > 0 &&
